d6f7cb94aa30cbb15887dc514f8ea8c8157f3b16
Rechner Fox <jkltechinc@gmail.com>
2020-12-22 14:53:20 -0800

    Update nkom url

--- hamfursbot/cron/nkom/refresh.sh
+++ hamfursbot/cron/nkom/refresh.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-DB_URL="https://www.nkom.no/frekvenser-og-elektronisk-utstyr/radioamator/_/attachment/download/5a85721a-223f-42ea-bd49-b71541d93815:c7aa67e268185b33eca25f50c56d31c5587eee69/Liste%20over%20norske%20radioamat%C3%B8rer%20(CSV).csv"
+DB_URL="https://www.nkom.no/frekvenser-og-elektronisk-utstyr/radioamator/_/attachment/download/5a85721a-223f-42ea-bd49-b71541d93815:4f27f874d8ef314474493bf52aec658d939d967d/Liste%20over%20norske%20radioamat%C3%B8rer%20(CSV).csv"
 wget -O import.csv $DB_URL && \
   ./fetch.py import.csv
 echo -n "Finished "
